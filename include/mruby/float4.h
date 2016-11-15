f6d651d74b1fc7e40e2121c3a457ef556a07662f
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-11-15 15:52:17 +0100

    C++ guard for C API

--- include/mruby/float4.h
+++ include/mruby/float4.h
@@ -6,6 +6,8 @@
 
 #include <mruby.h>
 
+MRB_BEGIN_DECL
+
 MRB_API struct mrb_data_type* mrb_float4_data_type();
 
 typedef struct mrb_float4_float
@@ -26,3 +28,5 @@ typedef struct mrb_float4_bool
 MRB_API mrb_bool mrb_float4_vec(mrb_state *mrb, mrb_value self, struct mrb_float4_float *value, mrb_int *size);
 MRB_API mrb_bool mrb_float4_ivec(mrb_state *mrb, mrb_value self, struct mrb_float4_int *value, mrb_int *size);
 MRB_API mrb_bool mrb_float4_bvec(mrb_state *mrb, mrb_value self, struct mrb_float4_bool *value, mrb_int *size);
+
+MRB_END_DECL
